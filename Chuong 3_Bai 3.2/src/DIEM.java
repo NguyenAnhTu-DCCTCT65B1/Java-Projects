cfb203fbb47e7bf127b961517f12c7901adaa043
nguyenanhtu <nguyenanhtu12062002@gmail.com>
2022-05-11 18:08:45 +0700

    Bai tap chuong 3

--- Chuong 3_Bai 3.2/src/DIEM.java
+++ Chuong 3_Bai 3.2/src/DIEM.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 
 public class DIEM {
+	
 	private int x, y;
-	public DIEM() {
-		System.out.print("Thong tin cua diem:");
-		Scanner d = new Scanner(System.in);
-		System.out.print("\nNhap diem x: ");
-		x = d.nextInt();
-		System.out.print("Nhap diem y: ");
-		y = d.nextInt();
+	
+	DIEM(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	DIEM() {
+		Scanner s = new Scanner(System.in);
+		x = s.nextInt();
+		y = s.nextInt();
+	}
+	
+	public void INDIEM() {
+		System.out.println("(" + this.x + ", " + this.y + ")");
+	}
+	
+	public static void main(String[] args) {
+		DIEM d = new DIEM();
+		d.INDIEM();
 	}
 }
-	 
--- Chuong 3_Bai 3.2/src/DUONGTRON.java
+++ Chuong 3_Bai 3.2/src/DUONGTRON.java
@@ -1,22 +1,50 @@
 import java.util.Scanner;
 
 public class DUONGTRON extends DIEM {
-    	double r;
-    	double pi = 3.14;
-    	public DUONGTRON() {
-    		System.out.print("\nThong tin cua hinh tron:");
-    		Scanner t = new Scanner(System.in);
-    		System.out.print("\nBan kinh duong tron: ");
-    		r = t.nextDouble();
-    	}
-    	public double CHUVI() {
-    		return r * 2 * pi;
-    	}
-    	public double DIENTICH() {
-    		return r * r * pi;
-    	}
-    	public void TINH() {
-    		System.out.print("Chu vi cua hinh tron là : " + CHUVI());
-    		System.out.print("\nDien tich cua hinh tron là : " + DIENTICH());
-    	}
-    }
+   private int r;
+   
+   DUONGTRON() {
+	   Scanner s = new Scanner(System.in);
+	   System.out.println("Nhap ban kinh hinh tron: ");
+	   this.r = s.nextInt();
+   }
+   
+   DUONGTRON(int x, int y, int r) {
+	   super(x, y);
+	   this.r = r;
+   }
+   
+   public void INDUONGTRON() {
+	   System.out.print("Toa do tam cua hinh tron la: ");
+	   super.INDIEM();
+	   System.out.println("Ban kinh cua hinh tron la: " + r);
+   }
+   
+   // Tinh chu vi
+   public double CHUVI() {
+	   return 2 * 3.14 * r;
+   }
+   
+   // Tinh dien tich
+   public double DIENTICH() {
+	   return 3.14 * r * r;
+   }
+   
+   public static void main(String[] args) {
+		System.out.println("Nhap toa do dinh va ban kinh hinh tron: ");
+		DUONGTRON dt = new DUONGTRON();
+		dt.INDUONGTRON();
+		dt.CHUVI();
+		System.out.println("Chu vi hinh tron la: " + dt.CHUVI());
+		dt.DIENTICH();
+		System.out.println("Dien tich hinh tron la: " + dt.DIENTICH());
+		
+		System.out.println("\nHinh tron thu 2: ");
+		DUONGTRON dt1 = new DUONGTRON(0, 0, 5);
+		dt1.INDUONGTRON();
+		dt1.CHUVI();
+		System.out.println("Chu vi hinh tron la: " + dt1.CHUVI());
+		dt.DIENTICH();
+		System.out.println("Dien tich hinh tron la: " + dt1.DIENTICH());
+   }
+}
